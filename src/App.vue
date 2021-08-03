fbf650e2c92181dcb77318fe735d3c08475d8a32
unalman <retaler_21@hotmail.com>
2021-08-03 09:01:06 +0300

    ilk commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{ name: 'EventList' }">Events</router-link> |
+    <router-link :to="{ name: 'About' }">About</router-link>
   </div>
   <router-view />
 </template>
@@ -26,5 +26,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4 {
+  font-size: 20px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import {
+  createRouter,
+  createWebHistory
+} from "vue-router";
+import EventList from "../views/EventList.vue";
+import About from "../views/About.vue";
+import EventDetails from "../views/EventDetails.vue";
 
-const routes = [
-  {
+const routes = [{
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "EventList",
+    component: EventList,
+  },
+  {
+    path: "/event/:id",
+    name: "EventDetails",
+    props:true,
+    component: EventDetails
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About
   },
 ];
 
